7bc2727d39edee095eba48300b49e18cfea4e14d
Rosyidmw <rosyidmktwbw@gmail.com>
2025-03-07 08:27:39 +0700

    first commit

---
+++ 02_Pengenalan_IDE_dan_Pemrograman_NodeJs/variable-local-global.js
@@ -0,0 +1,8 @@
+let globalVariable = "Rosyid Mukti Wibowo"
+
+function getMyIdentify() {
+    let localVariable = "2211104076"
+    console.log(`Nama saya ${globalVariable} dan NIM saya ${localVariable}`);
+}
+console.log(globalVariable);
+getMyIdentify();
---
+++ 02_Pengenalan_IDE_dan_Pemrograman_NodeJs/variables.js
@@ -0,0 +1,10 @@
+var name = "Rosyid Mukti Wibowo";
+const nim = 2211104076
+console.log(name);
+console.log("Halo nama saya " + name + " dan NIM saya " + nim);
+console.log(`Halo nama saya ${name} dan NIM saya ${nim}`);
+console.log(`Halo nama saya ${name} 
+    dan NIM saya ${nim}`);
+
+var name = "Rotibow";
+console.log(name);
